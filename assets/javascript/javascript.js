bead1db6a89773c117b2ca536eff5f55c27de76b
beardykraken <vcasmirri@gmail.com>
2020-02-11 20:46:16 -0500

    Added images for type symbols, added functionality for entering an in-game weather type and returning boosted types in an array of cards.

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,36 @@
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://pokemon-go1.p.rapidapi.com/weather_boosts.json",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "pokemon-go1.p.rapidapi.com",
+		"x-rapidapi-key": "137b0a8991mshfe042d583612905p1ff297jsnc05f2d857777"
+	}
+}
+
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+    $(".test").on("click", function() {
+        var weather = $("#boostedTest option:selected").text();
+        console.log(weather);
+        var currentBoost = response[weather];
+        for (i=0; i < currentBoost.length; i++) {
+            console.log("The boosted types are: " + currentBoost[i]);
+            var typeCard = $("<div>");
+            typeCard.addClass("card border-0 typeCard");
+            var typeSymbol = $("<img>");
+            typeSymbol.addClass("card-img-top typeSymbol");
+            typeSymbol.attr("src", "./assets/images/" + currentBoost[i] + ".png");
+            typeCard.append(typeSymbol);
+            var typeTitle = $("<div>");
+            typeTitle.addClass("card-title text-center");
+            typeTitle.text(currentBoost[i]);
+            typeCard.append(typeTitle);
+            $("#typeBox").append(typeCard);
+        }
+    })
+});
+
+
